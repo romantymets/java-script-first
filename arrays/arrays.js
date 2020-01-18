b05f5a4d1a859caaf55081d658e6542a1b685e30
romantymets <romantymets@gmail.com>
2020-01-18 22:16:16 +0200

    test commit

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,9 +1,24 @@
 const {  anyArray, tags, numberArrays, news  } = require('./data');
 
-// 1:  вивести всіх новини за допомогою for
-const array = [];
-// 2:  вивести всіх новини за допомогою forEach
-// 3:  замапити новини за допомогою map (додати нове поле tag === із масиву tags)
-// 4:  використати joint
-// 5: * відфільтрувати масив  numberArrays так щоб були тільки парні числа (.filter)
-// 6: * посортувати numberArrays (.sort)
+  // 1:  вивести всіх новини за допомогою for
+  const array = [];
+  // 2:  вивести всіх новини за допомогою forEach
+  // 3:  замапити новини за допомогою map (додати нове поле tag === із масиву tags)
+  // 4:  використати joint
+  // 5: * відфільтрувати масив  numberArrays так щоб були тільки парні числа (.filter)
+  // 6: * посортувати numberArrays (.sort)
+console.log('-----------1--------------')
+for(let i=0; i < news.length; i++){
+  const singleNew = news[i]
+  console.log(`------news[${i}] = `, singleNew)
+}
+console.log('-------------2----------')
+
+//forech
+news.forEach((singleNew, index) => {
+  const str = `-----news[${index}]`
+  console.log(str,singleNew)
+});
+numberArrays.forEach((value, ) => {
+  console.log(`array numbers`,value)
+})
