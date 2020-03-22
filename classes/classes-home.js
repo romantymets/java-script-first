f0e44d78c1c626498b9251eab9cd0fff081462f6
romantymets <romantymets@gmail.com>
2020-03-22 10:51:39 +0200

    init classes

--- classes/classes-home.js
+++ classes/classes-home.js
@@ -11,5 +11,39 @@
  *  Створити клас телефон із 5 властивостями і двома методами
  *  Створити клас android iPhone які наслідуються із класу телефон із 2 додатковими методами
  *  Створити 1 статичний мтод
- *  Створити гетер сетер 
+ *  Створити гетер сетер
  * */
+
+class Telephone {
+  constructor(brend) {
+    this.brend = brend;
+  }
+  color = 'red';
+  _displey ='tachskrin';
+    camera = '48 mp';
+      battery = '4000 AH'
+       simcard = 2;
+
+      present() {
+    console.log('I have a ', this.brend)
+  }
+operationSystem (opname){
+        this.opname = opname;
+        console.log('My op is', this.opname)
+}
+}
+const samsung = new Telephone('Samsung');
+console.log(samsung.present('Samsung'));
+console.log(samsung.operationSystem('Android'));
+
+const iphone = new Telephone('Iphone');
+console.log(iphone.present('Iphone'));
+console.log(iphone.operationSystem('IOS'));
+
+
+class Iphone extends Telephone {
+  constructor(brend){
+    super(brend)
+  }
+
+}
